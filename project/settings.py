787cdb2e19d6541f99bae7126fbdfd7a209f62c0
samverh <samverhezen@hotmail.com>
2023-04-26 14:39:26 +0200

    decrease project size for vercel

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app'] # Allow *.vercel.app
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app'] # Allow *.vercel.app
 DATABASES = {} # Prevent Django from loading an adapter
 
 
